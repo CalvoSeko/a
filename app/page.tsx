acb574f7f9a2494d794d646db7a659464802595b
Tianwei <90248677+CalvoSeko@users.noreply.github.com>
2024-11-06 20:44:28 +0000

    5

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[1fr] min-h-screen p-8">
-      <main className="grid grid-cols-2 md:grid-cols-4 gap-4">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="flex gap-4 overflow-x-auto max-w-[600px] w-full pb-2">
           {[1, 2, 3, 4, 5, 6, 7, 8].map((num) => (
             <img
