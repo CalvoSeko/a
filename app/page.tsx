abf4ce6041a8cc4e8a73fb0536b3b9b22228dcf6
Tianwei <90248677+CalvoSeko@users.noreply.github.com>
2024-11-06 20:38:41 +0000

    2

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,17 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[1fr] min-h-screen p-8">
       <main className="grid grid-cols-2 md:grid-cols-4 gap-4">
-        {[...Array(8)].map((_, i) => (
-          <Image
-            key={i}
-            src={`/${i + 1}.jpg`}
-            alt={`Image ${i + 1}`}
-            width={500}
-            height={500}
-            className="w-full h-full object-cover rounded-lg"
-          />
-        ))}
+        <div className="flex gap-4 overflow-x-auto max-w-[600px] w-full pb-2">
+          {[1, 2, 3, 4, 5, 6, 7, 8].map((num) => (
+            <img
+              key={num}
+              src={`/${num}.jpg`}
+              alt={`Foto ${num}`}
+              className="h-32 w-auto object-cover"
+            />
+          ))}
+        </div>
       </main>
     </div>
   );
-}
 }
