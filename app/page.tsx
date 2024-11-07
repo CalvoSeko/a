62cd5329487b9ba1ac633b052efee0f900c0a206
Tianwei <90248677+CalvoSeko@users.noreply.github.com>
2024-11-07 12:56:56 +0000

    2

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
               width="853"
               height="480"
               src="https://www.youtube.com/embed/UvtkUjxVm7c"
-              frameborder="0"
+              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowfullscreen
             ></iframe>
